a03f837abc33592755863e8c3e7f040f14936b21
Aleksandar Ristic <aristic665@gmail.com>
2021-05-15 23:03:37 +0200

    Finished most of header

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
-console.log('Testing paths...');
+// Mobile menu
+const showEl = document.querySelectorAll('[data-hide]');
+
+document.querySelector('.header__btn')
+.addEventListener('click', function() {
+
+  this.classList.toggle('open');
+  
+  this.classList.contains('open') ? 
+  showEl.forEach(el => openAnim(el)) :
+  showEl.forEach(el => closeAnim(el))
+});
+
+function openAnim(el) {
+  el.classList.remove('fade-out', 'hidden');
+  el.classList.add('open', 'fade-in');
+}
+
+function closeAnim(el) {
+  el.classList.replace('fade-in', 'fade-out');
+
+  setTimeout(() => {
+    el.classList.replace('open', 'hidden');
+  }, 500);   
+}
